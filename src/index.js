eecdad13dc8f2d9310569c287bdfe353283bae03
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-06-18 13:02:21 +0200

    Implement user wins

--- src/index.js
+++ src/index.js
@@ -17,12 +17,13 @@ class Game {
       this.#currentGrid.disable();
     }
     this.#currentGrid.makeMove(this.#currentPlayer, cell);
-    this.checkForWinner();
-    this.#currentPlayer = this.#players[
-      (this.#players.indexOf(this.#currentPlayer) + 1) % this.#players.length
-    ];
-    this.#currentGrid = this.#grids[cell.id];
-    this.#currentGrid.enable();
+    if (!this.checkForWinner()) {
+      this.#currentPlayer = this.#players[
+        (this.#players.indexOf(this.#currentPlayer) + 1) % this.#players.length
+      ];
+      this.#currentGrid = this.#grids[cell.id];
+      this.#currentGrid.enable();
+    }
   }
 
   checkForWinner() {
@@ -33,9 +34,18 @@ class Game {
     );
     if (winningGrids) {
       for (const gridId of winningGrids) {
-        this.#grids[gridId].setWinningFlag();
+        const grid = this.#grids[gridId];
+        const boundingRect = grid.htmlElement.getBoundingClientRect();
+        const canvas = launchFireworks(9, 200, boundingRect.height / 1000);
+        canvas.style.top = boundingRect.top - boundingRect.height / 6 + "px";
+        canvas.style.left = boundingRect.left + "px";
+        document.body.append(canvas);
+        grid.setWinningFlag();
       }
-      throw "TODO: player wins";
+
+      Game.showDialog(this.#currentPlayer).append(launchFireworks(20, 150));
+
+      return false;
     }
   }
 
@@ -43,6 +53,32 @@ class Game {
     const frag = document.createDocumentFragment();
     frag.append(...this.#grids.map((grid) => grid.htmlElement));
     return frag;
+  }
+
+  static showDialog(player) {
+    if (!this.dialogElement) {
+      const celebrateWinnerBanner = document.createElement("dialog");
+      const h2 = document.createElement("h2");
+      celebrateWinnerBanner.winnerSymbol = document.createTextNode(
+        player.symbol
+      );
+      h2.append(celebrateWinnerBanner.winnerSymbol, " wins!");
+      const form = document.createElement("form");
+      form.method = "dialog";
+      const button = document.createElement("input");
+      button.type = "submit";
+      button.value = "New Game";
+      form.append(button);
+
+      form.addEventListener("submit", startNewGame);
+      celebrateWinnerBanner.append(h2, form);
+      document.body.append(celebrateWinnerBanner);
+      this.dialogElement = celebrateWinnerBanner;
+    } else {
+      this.dialogElement.winnerSymbol = player.symbol;
+    }
+    this.dialogElement.showModal();
+    return this.dialogElement;
   }
 }
 
@@ -175,7 +211,15 @@ class Grid extends Cell {
   }
 }
 
-const game = new Game();
+let game = new Game();
+
+function startNewGame() {
+  const newGame = new Game();
+  const main = document.querySelector("main");
+  while (main.firstChild) main.removeChild(main.firstChild);
+  main.append(newGame.htmlElement);
+  game = newGame;
+}
 
 function onclick(event) {
   game.makeMove(cellInstances.get(event.target));
